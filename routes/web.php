d2d6b166629f4d45c4eeff6ba9dd32b518c5ca26
M40 <modev.404@gmail.com>
2024-12-08 14:01:10 +0300

    admin controller & dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ClassController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ids/dashboard', [AdminController::class, 'dashboard']);
